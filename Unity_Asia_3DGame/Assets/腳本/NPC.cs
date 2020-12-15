15b45d91f850278887f04687b2eda6b787932ec5
xup6890609 <71679918+xup6890609@users.noreply.github.com>
2020-12-15 16:03:40 +0800

    新增程式

---
+++ Unity_Asia_3DGame/Assets/腳本/NPC.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class NPC : MonoBehaviour
+{
+ [Header("NPC資料")]
+ public NPCData data;
+ [Header("對話框")]
+ public GameObject dialouge;
+ [Header("對話內容")]
+ public Text textContent;
+
+ //<Summary> 判斷玩家是否進入感應區
+ public bool playerInArea;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name=="羅伯特")
+        { playerInArea =true;
+          Dialouge();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "羅伯特")
+        {
+            playerInArea = false;
+        }
+    }
+        private void Dialouge()
+        {
+        // print(data.dialougeA);      // 取得字串全部資料
+        // print(data.dialouge[0]);    // 取字串局部資料[編號]，從0始算
+
+        /// for 迴圈:重複字串資料,i是代稱
+
+        /// 語法：
+        //  for (int i = 0; i < 10; i++)
+        //  {
+        //    print("我是迴圈：" + i);
+        //  }
+        
+        // 字串(dialougeA).長度
+        for (int i = 0; i < data.dialougeA.Length; i++)
+        {
+            print(data.dialougeA[i]);
+        }
+        }
+    }
+
---
+++ Unity_Asia_3DGame/Assets/腳本/NPCData.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+[ CreateAssetMenu(fileName="NPC資料" ,menuName="選單/NPC資料")]
+
+public class NPCData : ScriptableObject
+{
+    [Header("第一段對話"),TextArea(1,6)]
+    public string dialougeA;
+    [Header("第二段對話"), TextArea(1, 6)]
+    public string dialougeB;
+    [Header("第三段對話"), TextArea(1, 6)]
+    public string dialougeC;
+    [Header("任務項目需求數量")]
+    public int count;
+    [Header("已取得的項目數量")]
+    public int countCurrent;
+
+}
